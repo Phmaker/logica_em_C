45d927bbac03b7a93ed43ff29987c9cf99701cb4
Pedro Henrique Cadete <162760498+Phmaker@users.noreply.github.com>
2024-11-17 20:38:24 -0300

    Create Geradordesenhas.c
    Crio uma gerador de senhas aleatórias utilizando logica de programação

---
+++ Geradordesenhas.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//crio uma função 
+void gerarSenha(int comprimento) {
+    
+    //Declaro os tipos de caracteres que ira conter nessa senha
+    char caracteres[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+    int tamanho = sizeof(caracteres) - 1;
+
+   //Crio o gerador de senha usando a logica do FOR
+    printf("Senha gerada: ");
+    for (int i = 0; i < comprimento; i++) {
+        printf("%c", caracteres[rand() % tamanho]);
+    }
+    printf("\n");
+}
+
+
+//Crio minha fução main
+int main() {
+    int comprimento;
+
+    // Inicializo o gerador
+    srand(time(0));
+
+
+	//Peço ao usuario para digitar o tamanho da sua senha desejada e logo em seguida salvo ela em uma variavel
+    printf("Digite o comprimento da senha desejada: ");
+    scanf("%d", &comprimento);
+
+    // Chamo a função que declarei no void para iniciar o gerador 
+    gerarSenha(comprimento);
+
+
+	//Utilizo para a tela ficar congelada, Substituindo o system("pause")
+    printf("Digite qualquer tecla...");
+    getchar();
+    
+    return 0;
+}
